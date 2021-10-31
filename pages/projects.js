99a0fb797d68b032e4d21fa5c2e86c3156866e9b
UwUssimo Robinson <uwussimo@icloud.com>
2021-10-31 21:08:43 +0500

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -13,42 +13,10 @@ const Projects = () => {
         />
 
         <Entry
-          title="Opus"
-          description="Minimal note-taking application"
-          image="https://res.cloudinary.com/dsdlhtnpw/image/upload/v1572672667/opus_ts9o1o.png"
-          href="https://github.com/pacocoursey/opus"
-          position="top"
-        />
-
-        <Entry
           title="Dusk"
           description="Simple application icons"
           image="https://res.cloudinary.com/dsdlhtnpw/image/upload/v1572672667/dusk_o7qcsa.png"
-          href="https://github.com/pacocoursey/dusk"
-          position="top"
-        />
-
-        <Entry
-          title="F2PRS"
-          description="Accurate hiscores for RuneScape"
-          image="https://res.cloudinary.com/dsdlhtnpw/image/upload/v1572672667/f2prs_se1f4c.png"
-          href="https://github.com/pacocoursey/f2prs"
-          position="top"
-        />
-
-        <Entry
-          title="Xi"
-          description="Monochrome Atom UI theme"
-          image="https://res.cloudinary.com/dsdlhtnpw/image/upload/v1572672666/xi_xti7x0.png"
-          href="https://github.com/pacocoursey/xi-ui"
-          position="top"
-        />
-
-        <Entry
-          title="EHP"
-          description="Design and Website for EHP"
-          image="https://res.cloudinary.com/dsdlhtnpw/image/upload/v1572672667/ehp_ibfrlz.png"
-          href="https://github.com/pacocoursey/ehp-rs"
+          href="https://dusk.now.sh"
           position="top"
         />
 
