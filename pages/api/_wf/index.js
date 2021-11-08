3158192ed73fb2ed327afde1217baa9727149c8c
UwUssimo Robinson <uwussimo@icloud.com>
2021-11-09 00:09:55 +0500

    Update index.js

--- pages/api/_wf/index.js
+++ pages/api/_wf/index.js
@@ -1,4 +1,4 @@
 export default async (req, res) => {
-  const link = "https://github.com/00010023/waifu.up/projects/1";
+  const link = "https://github.com/00010023/waifu.up/projects/1?fullscreen=true";
   return res.redirect(link);
 };
