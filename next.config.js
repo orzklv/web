24772a469c81fdda482fb9788712e747c112e4f3
UwUssimo Robinson <uwussimo@icloud.com>
2021-11-08 17:50:44 +0500

    adding waifu run

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/waifurun",
+        destination: "/api/_wf",
+        permanent: true,
+      },
+    ];
+  },
   typescript: {
     ignoreDevErrors: true
   }
