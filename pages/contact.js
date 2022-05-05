bf1ca9c844a440bd70ce7d82bce32f8d0a5606ef
UwUssimo Robinson <uwussimo@icloud.com>
2022-05-05 07:57:31 +0500

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -4,11 +4,19 @@ const Contact = () => {
   return (
     <Page title="Contact" footer={false} description="Get in touch.">
       <article>
-        <p>Get in touch.</p>
+        <p>Get in touch...</p>
 
         <blockquote>
           <a href="mailto:uwussimo@icloud.com?subject=Hello" className="reset">
             uwussimo@icloud.com
+          </a>
+        </blockquote>
+
+        <p>...or follow me on telegram.</p>
+
+        <blockquote>
+          <a href="https://t.me/s/uwublog" className="reset">
+            @uwublog
           </a>
         </blockquote>
       </article>
