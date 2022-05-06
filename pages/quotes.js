b92479c5278e66f5b174e274d69c95c27122dd19
UwUssimo Robinson <uwussimo@icloud.com>
2022-05-06 11:33:23 +0500

    trying to host api

--- pages/quotes.js
+++ pages/quotes.js
@@ -1,6 +1,5 @@
 import Page from '@components/page'
 import getMarkdown from '@lib/get-markdown'
-import Link from '@components/link' // eslint-disable-line
 
 const Quotes = ({ html }) => {
   return (
