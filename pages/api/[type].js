b6898516c06859a43e6310c33dbca06c1b7afc41
UwUssimo Robinson <uwussimo@icloud.com>
2022-05-06 12:06:07 +0500

    removed fs functions

--- pages/api/[type].js
+++ pages/api/[type].js
@@ -2,7 +2,6 @@ import design from '@data/design.json'
 import reading from '@data/reading.json'
 import music from '@data/music.json'
 import blog from '@data/blog.json'
-import getRawdown from '@lib/get-rawdown'
 
 export default async function handler(req, res) {
   console.log(req.query)
@@ -15,12 +14,6 @@ export default async function handler(req, res) {
       return res.status(200).json(design)
     case 'blog':
       return res.status(200).json(blog)
-    case 'ideas':
-      return res.status(200).json({content: await getRawdown('data/ideas.md')})
-    case 'quotes':
-      return res.status(200).json({content: await getRawdown('data/quotes.md')})
-    case 'words':
-      return res.status(200).json({content: await getRawdown('data/words.md')})
     default:
       return res.status(404).json({ message: 'Unknown service' })
   }
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,3 +1,3 @@
 export default async function handler(req, res) {
-    return res.status(404).json({ query: '/api/[blog, design, ideas, keyboard, music, quotes, reading, words]' })
+    return res.status(404).json({ query: '/api/[blog, design, keyboard, music,reading]' })
 }
