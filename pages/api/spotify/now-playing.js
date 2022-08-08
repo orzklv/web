d50e8f01147ea9a9bc589aab44fda636f6000af4
UwUssimo Robinson <uwussimo@icloud.com>
2022-08-08 15:01:23 +0500

    Update now-playing.js

--- pages/api/spotify/now-playing.js
+++ pages/api/spotify/now-playing.js
@@ -15,21 +15,12 @@ export default async (_, res) => {
   const albumImageUrl = song.item.album.images[0].url
   const songUrl = song.item.external_urls.spotify
 
-  return new Response(
-    JSON.stringify({
-      album,
-      albumImageUrl,
-      artist,
-      isPlaying,
-      songUrl,
-      title
-    }),
-    {
-      status: 200,
-      headers: {
-        'content-type': 'application/json',
-        'cache-control': 'public, s-maxage=60, stale-while-revalidate=30'
-      }
-    }
-  );
+  return res.status(200).json({
+    album,
+    albumImageUrl,
+    artist,
+    isPlaying,
+    songUrl,
+    title,
+  })
 }
