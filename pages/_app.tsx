96b39c2852e16b385945cc706d5e67de62aafde6
UwUssimo Robinson <uwussimo@icloud.com>
2022-08-14 00:33:14 +0500

    ignore component level

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,9 @@ class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
+
       <ThemeProvider disableTransitionOnChange defaultTheme="dark">
+        {/*// @ts-ignore*/}
         <Component {...pageProps} />
       </ThemeProvider>
     )
