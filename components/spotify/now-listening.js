0d7973acecce5df4035d66d32747f6c997dda7b5
Yūri Katsuki <yuri@katsuki.moe>
2023-01-15 19:49:07 +0500

    3 seconds of interval for each refresh

--- components/spotify/now-listening.js
+++ components/spotify/now-listening.js
@@ -1,12 +1,11 @@
 import useSWR from 'swr'
-import Link from 'next/link'
 import Entry from '@components/entry'
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 const Spotify = () => {
   const { data, error } = useSWR('/api/spotify/now-playing', fetcher, {
-    refreshInterval: 1000,
+    refreshInterval: 3000,
   })
 
   if (error)
