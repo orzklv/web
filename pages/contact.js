32ebe886b002aafd6079d6e31106297a8cf5aa6a
Yūri Katsuki <yuri@katsuki.moe>
2023-01-15 18:28:15 +0500

    updating favicons & contact details

--- pages/contact.js
+++ pages/contact.js
@@ -7,16 +7,16 @@ const Contact = () => {
         <p>Get in touch...</p>
 
         <blockquote>
-          <a href="mailto:uwussimo@icloud.com?subject=Hello" className="reset">
-            papa@uwussi.moe
+          <a href="mailto:yuri@katsuki.moe?subject=Hewwo Yuwi... Iwd liwe to tawk with ya!" className="reset">
+            yuri@katsuki.moe
           </a>
         </blockquote>
 
         <p>...or follow me on telegram.</p>
 
         <blockquote>
-          <a href="https://t.me/s/uwublog" className="reset">
-            @uwublog
+          <a href="https://t.me/s/yurionblog" className="reset">
+            @yurionblog
           </a>
         </blockquote>
       </article>
