617054d6ce2118f5195b7c734cb719dfbe54ff28
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-08-18 12:38:05 +0500

    move contents to blog repo

--- flake.nix
+++ flake.nix
@@ -3,49 +3,69 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    # nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/24.05";
 
-    utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-    outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
-    "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"
-  ] (system: let
-    pkgs = import nixpkgs {
-      inherit system;
+  outputs = { self, nixpkgs, flake-utils, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+          config.allowUnfree = true;
+        };
+      in
+      {
+        # Nix script formatter
+        formatter = pkgs.nixpkgs-fmt;
 
-      overlays = [];
+        devShells.default = pkgs.mkShell rec {
+          name = "webnya";
 
-      # config.allowUnfree = true;
-    };
-  in {
-    devShells.default = pkgs.mkShell rec {
-      name = "webnya";
+          packages = with pkgs; [
+            zola
+            lolcat
+            figlet
+          ];
 
-      packages = with pkgs; [
-        zola
-        lolcat
-        figlet
-      ];
+          shellHook =
+            let
+              icon = "f121";
+            in
+            ''
+              export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
 
-      shellHook = let
-        icon = "f121";
-      in ''
-        export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+              # Show some nyanny text
+              figlet -f slant "${name}" | lolcat
+              printf "\n\n"
 
-        figlet -f slant "${name}" | lolcat
-        printf "\n\n"
-        zola serve &
-        SERVER_PID=$!
+              # Check if the blog repo exists
+              # If not, clone it from GitHub
+              if [ ! -d ../blog ]; then
+                git clone https://github.com/orzklv/blog.git ../blog
+              fi
 
-        finish()
-        {
-          printf "\nTime to say goodbye, Nya!\n" | lolcat
-          kill $SERVER_PID
-        }
+              # Symlink contents to blog repo
+              ln -s ../blog/content ./content
 
-        trap finish EXIT
-      '';
-    };
-  });
+              zola serve &
+              SERVER_PID=$!
+
+              finish()
+              {
+                printf "\nTime to say goodbye, Nya!\n" | lolcat
+
+                # Kill the server
+                kill $SERVER_PID
+
+                # Remove symlink
+                rm ./content
+              }
+
+              trap finish EXIT
+            '';
+        };
+      });
 }
